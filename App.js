3cbfcb3baf61faabed9c4140d0111324d6f51de2
iamsaurabh <bagdesaurabh69@gmail.com>
2020-06-16 17:56:47 +0530

    Done

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoitem';
+import AddTodo from './components/addTodo'
 
 export default function App() {
   const [todos, setTodos]= useState([
@@ -15,22 +16,44 @@ export default function App() {
       return prevTodos.filter(todo=> todo.key != key);
     })
   } 
-  return (
-    <View style={styles.container}>
-      <Header/>
-      <View style={styles.content}>
-        {/* {Todo form} */}
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem={({item})=> (
-              <TodoItem item={item} pressHandler={pressHandler}/> 
-            )}
-          />
-        </View>
-      </View>
 
-    </View>
+  const submitHandler= text =>{
+
+    if(text.length > 3){
+      setTodos(prevTodos => {
+        return [
+          {text, key: Math.random().toString()},
+          ...prevTodos
+        ]
+      });
+    }else {
+      Alert.alert('Opps!', 'Todos should be more than 3 chars long',[
+        {text: 'Understood', onPress: ()=> console.log('alert closed')}
+      ])
+    }
+    
+  }
+  return (
+    <TouchableWithoutFeedback onPress={()=>{
+      Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.container}>
+        <Header/>
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler}/>
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({item})=> (
+                <TodoItem item={item} pressHandler={pressHandler}/> 
+              )}
+            />
+          </View>
+        </View>
+
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -41,8 +64,11 @@ const styles = StyleSheet.create({
   },
   content: {
     padding : 40, 
+    backgroundColor: 'pink',
+    flex: 1
   },
   list: {
+    flex:1,
     margin: 20
   }
 });
